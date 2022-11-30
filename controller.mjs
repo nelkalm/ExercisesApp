7d7a29316b334c508cdbd2d833556f5ec59a86cb
Nelson Lu <nelkalmlu@gmail.com>
2022-11-30 15:51:54 -0800

    backend set up complete

--- controller.mjs
+++ controller.mjs
@@ -6,24 +6,144 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
+/**
+ * Create an exercise with a POST request
+ */
 app.post("/exercises", (req, res) => {
-  res.send("Create using POST /exercises");
+  exercises
+    .createExercise(
+      req.body.name,
+      req.body.reps,
+      req.body.weight,
+      req.body.unit,
+      req.body.date
+    )
+    .then((exercise) => {
+      res.status(201).json(exercise);
+    })
+    .catch((error) => {
+      console.error(error);
+      // Send a message if the request had an error.
+      res.status(400).json({
+        Error: "Data is invalid or data is missing for required fields.",
+      });
+    });
+  // res.send("Create using POST /exercises");
 });
 
+/**
+ * Retrieve all exercises or a subset of exercises with a GET request
+ */
 app.get("/exercises", (req, res) => {
-  res.send("Read using GET /exercises");
+  let filter = {};
+  // Set more filter options here
+  if (req.query.date !== undefined) {
+    filter = { date: req.query.date };
+  }
+  exercises
+    .findExercises(filter, "", 0)
+    .then((exercises) => {
+      res.send(exercises);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send({ Error: "Something's wrong with fetching all exercises." });
+    });
+  // res.send("Read using GET /exercises");
 });
 
-app.get("/exercises/:id", (req, res) => {
-  res.send("GET using GET /exercises/:id");
+/**
+ * Retrieve a single exercise based on ID with a GET request
+ */
+app.get("/exercises/:_id", (req, res) => {
+  const exerciseId = req.params._id;
+  exercises
+    .findExerciseById(exerciseId)
+    .then((exercise) => {
+      if (exercise !== null) {
+        res.json(exercise);
+      } else {
+        res.status(404).json({ Error: "Exercise not found" });
+      }
+    })
+    .catch((error) => {
+      res
+        .status(400)
+        .json({ Error: "Something's wrong with finding the exercise." });
+    });
+  // res.send("GET using GET /exercises/:id");
 });
 
-app.put("/exercises/:id", (req, res) => {
-  res.send("PUT using GET /exercises/:id");
+/**
+ * Update an exercise based on ID with a PUT request
+ */
+app.put("/exercises/:_id", (req, res) => {
+  exercises
+    .updateExercise(
+      req.params._id,
+      req.body.name,
+      req.body.reps,
+      req.body.weight,
+      req.body.unit,
+      req.body.date
+    )
+    .then((modifiedCount) => {
+      // Validation
+      if (
+        !req.body.name ||
+        !req.body.reps ||
+        !req.body.weight ||
+        !req.body.unit ||
+        !req.body.date
+      ) {
+        res
+          .status(404)
+          .json({ Error: "At least one required field is missing." });
+      }
+      if (req.body.name.length < 1) {
+        res.status(404).json({ Error: "Invalid name field." });
+      }
+      // Update document
+      if (modifiedCount === 1) {
+        res.json({
+          _id: req.params._id,
+          name: req.body.name,
+          reps: req.body.reps,
+          weight: req.body.weight,
+          unit: req.body.unit,
+          date: req.body.date,
+        });
+      } else {
+        res.status(404).json({ Error: "Exercise not found" });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res
+        .status(400)
+        .json({ Error: "Something's wrong with updating an exercise." });
+    });
+  // res.send("PUT using GET /exercises/:id");
 });
 
-app.delete("/exercises/:id", (req, res) => {
-  res.send("DELETE using GET /exercises/:id");
+/**
+ * Delete an exercise based on ID with a DELETE request
+ */
+app.delete("/exercises/:_id", (req, res) => {
+  exercises
+    .deleteExercise(req.params._id)
+    .then((deletedCount) => {
+      if (deletedCount === 1) {
+        res.status(204).send();
+      } else {
+        res.status(404).json({ Error: "Exercise not found" });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send({ error: "Something's wrong with deleting an exercise." });
+    });
+  // res.send("DELETE using GET /exercises/:id");
 });
 
 app.listen(PORT, () => {
--- model.mjs
+++ model.mjs
@@ -5,12 +5,22 @@ import "dotenv/config";
 // specified in MONGODB_CONNECT_STRING
 mongoose.connect(process.env.MONGODB_CONNECT_STRING, { useNewUrlParser: true });
 const db = mongoose.connection;
+db.once("open", (err) => {
+  if (err) {
+    res.status(500).json({ error: "500:Connection to the server failed." });
+  } else {
+    console.log(
+      "Successfully connected to MongoDB Movies collection using Mongoose."
+    );
+  }
+});
 
 // Schema definition
 const exerciseSchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minLength: 1,
   },
   reps: {
     type: Number,
@@ -27,6 +37,7 @@ const exerciseSchema = mongoose.Schema({
   unit: {
     type: String,
     required: true,
+    enum: ["lbs", "lbs", "miles", "m", "km"],
     default: "lbs",
   },
   date: {
@@ -36,12 +47,56 @@ const exerciseSchema = mongoose.Schema({
   },
 });
 
-db.once("open", (err) => {
-  if (err) {
-    res.status(500).json({ error: "500:Connection to the server failed." });
-  } else {
-    console.log(
-      "Successfully connected to MongoDB Movies collection using Mongoose."
-    );
-  }
-});
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+// Create an exercise
+const createExercise = async (name, reps, weight, unit, date) => {
+  const exercise = new Exercise({
+    name: name,
+    reps: reps,
+    weight: weight,
+    unit: unit,
+    date: date,
+  });
+  return exercise.save();
+};
+
+// Retrieve
+const findExercises = async (filter) => {
+  const query = Exercise.find(filter);
+  return query.exec();
+};
+
+const findExerciseById = async (_id) => {
+  const query = Exercise.findById(_id);
+  return query.exec();
+};
+
+// Update
+const updateExercise = async (_id, name, reps, weight, unit, date) => {
+  const result = await Exercise.replaceOne(
+    { _id: _id },
+    {
+      name: name,
+      reps: reps,
+      weight: weight,
+      unit: unit,
+      date: date,
+    }
+  );
+  return result.modifiedCount;
+};
+
+// Delete
+const deleteExercise = async (_id) => {
+  const result = await Exercise.deleteOne({ _id: _id });
+  return result.deletedCount;
+};
+
+export {
+  createExercise,
+  findExerciseById,
+  findExercises,
+  updateExercise,
+  deleteExercise,
+};
